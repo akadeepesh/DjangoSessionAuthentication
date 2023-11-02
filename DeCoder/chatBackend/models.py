64c76749968a51952745ca8e4da463cff8952966
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-03 04:14:03 +0530

    added relevant models

--- DeCoder/chatBackend/models.py
+++ DeCoder/chatBackend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.firstname
