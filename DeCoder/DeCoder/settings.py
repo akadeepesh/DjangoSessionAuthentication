69bc41aa382176420b21b7800c3b608b9c635534
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-03 05:06:52 +0530

    added to allowed hosts

--- DeCoder/DeCoder/settings.py
+++ DeCoder/DeCoder/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
