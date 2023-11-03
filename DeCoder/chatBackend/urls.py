f8fe218ce1d3f927bb2e0d2d71fe54755a4e5990
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-03 11:37:20 +0530

    added login authentication

--- DeCoder/chatBackend/urls.py
+++ DeCoder/chatBackend/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import UserViewSet, login
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("login/", login, name="login"),
 ]
--- DeCoder/chatBackend/views.py
+++ DeCoder/chatBackend/views.py
@@ -1,8 +1,23 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+    user = authenticate(email=email, password=password)
+
+    if user is None:
+        return Response({"error": "Invalid login credentials"})
+
+    return Response({"message": "Logged in successfully"})
