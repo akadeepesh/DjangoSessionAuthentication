9ae3b94a075ded4a92959c7d7105a13e658d50f1
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-03 23:19:15 +0530

    new login function

--- DeCoder/chatBackend/views.py
+++ DeCoder/chatBackend/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
-from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# from django.contrib.auth import authenticate
+# from rest_framework.decorators import api_view
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,15 +14,37 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+# @api_view(["POST"])
+# def login(request):
+#     email = request.data.get("email")
+#     password = request.data.get("password")
+#     print(email, password)
+#     user = authenticate(request, username=email, password=password)
+
+#     if user is None:
+#         print("User not found")
+#         return Response({"error": "Invalid login credentials"})
+
+#     return Response({"message": "Logged in successfully"})
+
+
 @api_view(["POST"])
 def login(request):
     email = request.data.get("email")
     password = request.data.get("password")
-    print(email, password)
-    user = authenticate(request, username=email, password=password)
 
-    if user is None:
-        print("User not found")
-        return Response({"error": "Invalid login credentials"})
+    # Perform authentication logic here, such as:
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response(
+            "Invalid email or password", status=status.HTTP_401_UNAUTHORIZED
+        )
 
-    return Response({"message": "Logged in successfully"})
+    if user.password == password:
+        # User authenticated successfully
+        return Response("Login successful", status=status.HTTP_200_OK)
+    else:
+        return Response(
+            "Invalid email or password", status=status.HTTP_401_UNAUTHORIZED
+        )
