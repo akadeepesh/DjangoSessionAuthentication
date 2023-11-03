0ba9ed7280d6a570b28fe77d16ddf8eb73d846a5
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-11-03 23:40:21 +0530

    done login

--- DeCoder/DeCoder/settings.py
+++ DeCoder/DeCoder/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:3000", "http://127.0.0.1:3000", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "127.0.0.1",
+    "localhost",
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
